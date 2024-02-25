236194b05d2a4906c017e95fe53bf31938d0e6a2
BangYunseo <yunseobang33@gmail.com>
2024-02-25 22:00:37 +0900

    Base Product
    게임에 필요한 요소 일부 추가 및 설정 완료

---
+++ BulletAvoid/Assets/Scenes/GameController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject FoodPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Food;
+        Food = Instantiate(FoodPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        print("Instantiate Food complete!")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

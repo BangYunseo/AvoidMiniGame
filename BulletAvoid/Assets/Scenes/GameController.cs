e58a5fd32c94d207dbf0fa3cee0bc6c5fc533772
BangYunseo <yunseobang33@gmail.com>
2024-03-22 21:51:59 +0900

    수정
    time - > score 변경 완료
    디자인 약간 추가
    버튼 수정 완료
    score 수정 완료

--- BulletAvoid/Assets/Scenes/GameController.cs
+++ BulletAvoid/Assets/Scenes/GameController.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject FoodPrefab;
+    public GameObject SharkPrefab;
     public GameObject uiStartGameObject;
     public GameObject uiEndGameObject;
-    public Text uiTime;
+    public Text uiScore;
     
-    int sec;
+    int score;
     // Start is called before the first frame update
     
     void Start()
@@ -19,34 +19,35 @@ public class GameController : MonoBehaviour
     }
 
     public void PressStart(){
-        sec = 0;
+        score = 0;
+        uiScore.text = "SCORE : " + score;
 
         uiStartGameObject.SetActive(false);
 
-        InvokeRepeating("MakeFood", 0f, 1f);
-        InvokeRepeating("SetTime", 1f, 1f);
+        InvokeRepeating("MakeShark", 0f, 0.5f);
+        InvokeRepeating("SetScore", 1f, 1f);
     }
 
     public void PressRestart(){
-        sec = 0;
-        uiTime.text = "" + sec;
+        score = 0;
+        uiScore.text = "SCORE : " + score;
 
-        GameObject[] Foods = GameObject.FindGameObjectsWithTag("Food");
+        GameObject[] Sharks = GameObject.FindGameObjectsWithTag("Shark");
 
-        foreach (GameObject food in Foods){
-            Destroy(food);
+        foreach (GameObject Shark in Sharks){
+            Destroy(Shark);
         }
         
         uiEndGameObject.SetActive(false);
 
     }
 
-    void SetTime(){
-        sec = sec + 1;
-        uiTime.text = sec.ToString();
+    void SetScore(){
+        score++;
+        uiScore.text = "SCORE : " + score.ToString();
     }
-    void MakeFood() {
-        GameObject Food;
+    void MakeShark() {
+        GameObject Shark;
 
         float switchV = Random.value;
         float xV = Random.Range(-13f, 13f);
@@ -54,18 +55,18 @@ public class GameController : MonoBehaviour
 
         if(switchV > 0.5f) {
             if(Random.value > 0.5f){
-                Food = Instantiate(FoodPrefab, new Vector3(-13f, yV, 0f), Quaternion.identity);
+                Shark = Instantiate(SharkPrefab, new Vector3(-13f, yV, 0f), Quaternion.identity);
             }
             else{
-                Food = Instantiate(FoodPrefab, new Vector3(13f, yV, 0f), Quaternion.identity);
+                Shark = Instantiate(SharkPrefab, new Vector3(13f, yV, 0f), Quaternion.identity);
             }
         }
         else {
             if(Random.value > 0.5f){
-                Food = Instantiate(FoodPrefab, new Vector3(xV, -22f, 0f), Quaternion.identity);
+                Shark = Instantiate(SharkPrefab, new Vector3(xV, -22f, 0f), Quaternion.identity);
             }
             else{
-                Food = Instantiate(FoodPrefab, new Vector3(xV, 22f, 0f), Quaternion.identity);
+                Shark = Instantiate(SharkPrefab, new Vector3(xV, 22f, 0f), Quaternion.identity);
             }
         }
     }
--- BulletAvoid/Assets/Scenes/Player.cs
+++ BulletAvoid/Assets/Scenes/Player.cs
@@ -5,45 +5,112 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     string dir;
+    float minX, maxX, minY, maxY;
+    float verticalSpeed = 0f; // 수직 속도
+    float horizontalSpeed = 0f; // 수평 속도
+    float speed = 0.1f;
+    float padding = 0.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 minBound = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 maxBound = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        minX = minBound.x + padding;
+        maxX = maxBound.x - padding;
+        minY = minBound.y + padding;
+        maxY = maxBound.y - padding;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(dir == "up"){
-            transform.Translate(Vector3.up * 0.1f);
-        }
-        else if(dir == "left"){
-            transform.Translate(Vector3.left * 0.1f);
-        }
-        else if(dir == "right"){
-            transform.Translate(Vector3.right * 0.1f);
-        }
-        else if(dir == "down"){
-            transform.Translate(Vector3.down * 0.1f);
-        }
+        // Mobile
+        MovePlayerMobile();
+
+        // PC
+        MovePlayerPC();
     }
-    public void Press_normal(){
-        dir = "null";
+
+    void MovePlayerMobile()
+    {
+        Vector3 newPos = transform.position;
+
+        if (dir == "up")
+        {
+            verticalSpeed += speed * Time.deltaTime;
+            newPos += Vector3.up * speed;
+        }
+        else if (dir == "left")
+        {
+            horizontalSpeed -= speed * Time.deltaTime;
+            newPos += Vector3.left * speed;
+        }
+        else if (dir == "right")
+        {
+            horizontalSpeed += speed * Time.deltaTime;
+            newPos += Vector3.right * speed;
+        }
+        else if (dir == "down")
+        {
+            verticalSpeed -= speed * Time.deltaTime;
+            newPos += Vector3.down * speed;
+        }
+        
+        // 새로운 위치를 화면 경계 내에 고정
+        newPos.x = Mathf.Clamp(newPos.x, minX, maxX);
+        newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
+
+        // 캐릭터 위치 업데이트
+        transform.position = newPos;
     }
-    public void Press_up(){
+
+    void MovePlayerPC()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // 이동 방향 설정
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+
+        // 이동량에 이동 속도를 곱하여 속도 조절
+        movement *= speed;
+
+        // 현재 위치에 이동량을 더하여 새로운 위치 계산
+        Vector3 newPos = transform.position + movement;
+
+        // 새로운 위치를 화면 경계 내에 고정
+        newPos.x = Mathf.Clamp(newPos.x, minX, maxX);
+        newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
+
+        // 캐릭터 위치 업데이트
+        transform.position = newPos;
+    }
+
+    public void Press_normal()
+    {
+        dir = "NULL";
+    }
+    public void Press_up()
+    {
         dir = "up";
     }
-    public void Press_down(){
+    public void Press_down()
+    {
         dir = "down";
     }
-    public void Press_left(){
+    public void Press_left()
+    {
         dir = "left";
     }
-    public void Press_right(){
+    public void Press_right()
+    {
         dir = "right";
     }
 
-    void OnTriggerEnter2D(Collider2D collider){
+    void OnTriggerEnter2D(Collider2D collider)
+    {
         Debug.Log(collider.gameObject);
         GameObject.Find("game").GetComponent<GameController>().uiEndGameObject.SetActive(true);
     }
